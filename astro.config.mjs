8d400688cd5f5c57cab35ef0be7d38d47c960563
john.dev <johninquest@gmail.com>
2026-01-06 00:21:46 +0100

    rolled backed to netlify hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://johninquest.github.io/scm-website',
     devToolbar: {
         enabled: false
     },
