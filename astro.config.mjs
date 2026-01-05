80cc92d69034096dd779bf40d3cfd5b1738312a6
john.dev <johninquest@gmail.com>
2026-01-05 23:47:06 +0100

    trying to fix github to custom domain routing issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://sozialcm.de',
+    site: 'https://johninquest.github.io/scm-website',
     devToolbar: {
         enabled: false
     },
